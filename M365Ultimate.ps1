bd466b3263e5af064e9803c4159d9ad9ebcd1adc
Jeff Jones <spjeff@spjeff.com>
2020-12-04 15:56:11 -0600

    Latest

--- M365Ultimate.ps1
+++ M365Ultimate.ps1
@@ -1,6 +1,6 @@
 <#
 .DESCRIPTION
-	Loads all Microsoft 365 cloud PowerShell modules in a single window.  No more wasted time jumping between download pages and locating various Import-Module from each product team.  Collected all here centrally to provide M365 admins with a single PowerShell PS1 to install and import every command they might need.  Enables cloud admin productivity to quickly access every command.  Cheers.
+	Loads all Microsoft 365 cloud PowerShell modules in a single window.  No more wasted time jumping between download pages and locating various Install-Module from each product team.  Collected all here centrally to provide M365 admins with a single PowerShell PS1 to install and import every command they might need.  Enables cloud admin productivity to quickly access every command.  Cheers.
 
 	* Based on https://docs.microsoft.com/en-us/microsoft-365/enterprise/connect-to-all-microsoft-365-services-in-a-single-windows-powershell-window
 	* Extended with additional community modules
@@ -12,7 +12,7 @@
 	File Name:  M365Ultimate.ps1
 	Author   :  Jeff Jones  @spjeff
 	Version  :  1.0.1
-	Modified :  2020-11-29
+	Modified :  2020-12-04
 
 .LINK
 	https://github.com/spjeff/m365ultimate
@@ -24,7 +24,8 @@ Start-Transcript
 Set-ExecutionPolicy "RemoteSigned"
 
 # Azure Active Directory (from https://docs.microsoft.com/en-us/microsoft-365/enterprise/connect-to-microsoft-365-powershell)
-Install-Module -Name "AzureAD"
+#REM Install-Module -Name "AzureAD"
+Install-Module -Name "AzureADPreview"
 
 # Exchange Online (from https://docs.microsoft.com/en-us/powershell/exchange/exchange-online-powershell-v2?view=exchange-ps#install-and-maintain-the-exo-v2-module)
 Install-Module -Name "ExchangeOnlineManagement"
@@ -33,25 +34,25 @@ Install-Module -Name "ExchangeOnlineManagement"
 #DEPRECATED Install-Module -Name "msonline"
 
 # SPO SharePoint Online (from https://docs.microsoft.com/en-us/powershell/sharepoint/sharepoint-online/connect-sharepoint-online)
-Import-Module -Name "Microsoft.Online.SharePoint.PowerShell"
+Install-Module -Name "Microsoft.Online.SharePoint.PowerShell"
 
 # PNP Patterns and Practice (from https://docs.microsoft.com/en-us/powershell/sharepoint/sharepoint-pnp/sharepoint-pnp-cmdlets)
-Import-Module -Name "SharePointPnPPowerShellOnline"
+Install-Module -Name "SharePointPnPPowerShellOnline"
 
 # Teams (from https://docs.microsoft.com/en-us/microsoftteams/teams-powershell-install)
-Import-Module -Name "MicrosoftTeams"
+Install-Module -Name "MicrosoftTeams"
 
 # Data Gateway (from https://docs.microsoft.com/en-us/powershell/gateway/overview?view=datagateway-ps)
 Install-Module -Name "DataGateway"
 
 # PowerBI (from https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps)
-Import-Module -Name "MicrosoftPowerBIMgmt"
-Import-Module -Name "MicrosoftPowerBIMgmt.Admin"
-Import-Module -Name "MicrosoftPowerBIMgmt.Capacities"
-Import-Module -Name "MicrosoftPowerBIMgmt.Data"
-Import-Module -Name "MicrosoftPowerBIMgmt.Profile"
-Import-Module -Name "MicrosoftPowerBIMgmt.Reports"
-Import-Module -Name "MicrosoftPowerBIMgmt.Workspaces"
+Install-Module -Name "MicrosoftPowerBIMgmt"
+Install-Module -Name "MicrosoftPowerBIMgmt.Admin"
+Install-Module -Name "MicrosoftPowerBIMgmt.Capacities"
+Install-Module -Name "MicrosoftPowerBIMgmt.Data"
+Install-Module -Name "MicrosoftPowerBIMgmt.Profile"
+Install-Module -Name "MicrosoftPowerBIMgmt.Reports"
+Install-Module -Name "MicrosoftPowerBIMgmt.Workspaces"
 
 # PowerApps (from https://docs.microsoft.com/en-us/power-platform/admin/powerapps-powershell#powerapps-cmdlets-for-app-creators-preview)
 # includes PowerAutomate "MS Flow" (from https://docs.microsoft.com/en-us/power-platform/admin/powerapps-powershell#power-automate-commands)
